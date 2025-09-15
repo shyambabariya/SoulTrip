4cde413c90cb75807ffacd998d2b1905b4470556
@MaD Max <shyambabariyaa@gmail.com>
2025-09-15 19:11:36 +0530

    default image added

--- models/listing.js
+++ models/listing.js
@@ -10,6 +10,7 @@ const listingSchema = new Schema({
     },
     image:{
         type:String,
+        set: (v) => (v === "") ? "https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
     },
     price:{
         type:Number,
