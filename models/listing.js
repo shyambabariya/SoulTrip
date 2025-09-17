cdaf036d92bc5d4a15d727029ae49d2f0c41bd4d
@MaD Max <shyambabariyaa@gmail.com>
2025-09-17 11:02:47 +0530

    review array added

--- models/listing.js
+++ models/listing.js
@@ -1,28 +1,43 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 const listingSchema = new Schema({
-    title:{
-        type:String,
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
     },
-    description:{
-        type:String,
-    },
-    image:{
-        type:String,
-        set: (v) => (v === "") ? "https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
-    },
-    price:{
-        type:Number,
-    },
-    location:{
-        type:String,
-    },
-    country:{
-        type:String,
-    },
+  ],
+});
+
+listingSchema.post("findOneAndDelete", async (listing) =>{
+    if(listing){
+        await Review.deleteMany({_id:{$in: listing.reviews}});
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
-
