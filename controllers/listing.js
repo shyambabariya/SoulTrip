16aee49d65593391da9809a33f8082acb42d901e
@MaD Max <shyambabariyaa@gmail.com>
2025-09-24 21:52:27 +0530

    listing controller

---
+++ controllers/listing.js
@@ -0,0 +1,68 @@
+const Listing = require("../models/listing");
+
+module.exports.index = async (req, res) => {
+  let listings = await Listing.find({});
+  res.render("listings/index.ejs", { listings });
+};
+
+module.exports.renderNewForm = (req, res) => {
+  res.render("listings/new.ejs");
+};
+
+module.exports.createListing = async (req, res) => {
+  // let {title, description, image,price,location,country} = req.body;
+  // let newListing = new Listing({
+  //     title:title,
+  //     description:description,
+  //     image:image,
+  //     price:price,
+  //     location:location,
+  //     country:country,
+  // });
+  // console.log(newListing);
+  //Above way is more longer so we use shorter way to do the same things
+
+  // for it we use object's key:value pair
+  let newListing = new Listing(req.body.listing);
+  newListing.owner = req.user._id;
+  await newListing.save();
+  req.flash("success", "New Listing Added Successfully!");
+  res.redirect("/listings");
+};
+
+module.exports.showListing = async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("owner");
+  if (!listing) {
+    req.flash("error", "Listing you requested for does not exist!");
+    return res.redirect("/listings");
+  }
+  res.render("listings/show.ejs", { listing });
+};
+
+module.exports.renderEditForm = async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  if (!listing) {
+    req.flash("error", "Listing you requested for does not exist!");
+    return res.redirect("/listings");
+  }
+  res.render("listings/edit.ejs", { listing });
+};
+
+module.exports.updateListing = async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  req.flash("success", "Listing Updated Successfully!");
+  res.redirect(`/listings/${id}`);
+};
+
+module.exports.destroyListing = async (req, res) => {
+  let { id } = req.params;
+  let delResult = await Listing.findByIdAndDelete(id);
+  console.log(delResult);
+  req.flash("success", "Listing Deleted Successfully!");
+  res.redirect("/listings");
+};
