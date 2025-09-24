838b773c643a06b0f8dc14aa5671b8005c8df3b0
@MaD Max <shyambabariyaa@gmail.com>
2025-09-24 21:51:47 +0530

    router.route added

--- routes/user.js
+++ routes/user.js
@@ -2,56 +2,43 @@ const express = require("express");
 const router = express.Router();
 const ExpressError = require("../utils/ExpressError");
 const wrapAsync = require("../utils/wrapAsync");
-const User = require("../models/user");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware");
+const userController = require("../controllers/user");
 
-router.get("/signup", (req, res) => {
-  res.render("user/signup.ejs");
-});
+// router.get("/signup", userController.signupForm);
 
-router.post(
-  "/signup",
-  wrapAsync(async (req, res) => {
-    try{
-        let { username, email, password } = req.body;
-        let newUser = new User({
-          username,
-          email,
-        });
-        const registerdUser = await User.register(newUser, password);
-        req.login(registerdUser, (err)=>{
-          if(err){
-            next(err);
-          }
-          req.flash("success", "Welcome to SoulTrip!");
-          res.redirect("/listings");
-        })
-    }catch(e){
-        req.flash("error", e.message);
-        res.redirect("/signup");
-    }
-  })
-);
+router
+  .route("/signup")
+  .get(userController.signupForm)
+  .post(wrapAsync(userController.signup));
 
-router.get("/login", (req, res) => {
-  res.render("user/login.ejs");
-});
+// router.post("/signup", wrapAsync(userController.signup));
 
-router.post("/login",saveRedirectUrl,passport.authenticate("local", { failureRedirect: '/login', failureFlash:true }),async (req, res)=>{
-    req.flash("success", "Welcome Back to SoulTrip!");
-    let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-});
+// router.get("/login", userController.loginForm);
 
-router.get("/logout", (req,res)=>{
-    req.logout((err)=>{
-        if(err){
-            return next(err);
-        }
-        req.flash("success","Logged out Successfully");
-        res.redirect("/listings");
-    });
-});
+router
+  .route("/login")
+  .get(userController.loginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    userController.login
+  );
+
+// router.post(
+//   "/login",
+//   saveRedirectUrl,
+//   passport.authenticate("local", {
+//     failureRedirect: "/login",
+//     failureFlash: true,
+//   }),
+//   userController.login
+// );
+
+router.get("/logout", userController.logout);
 
 module.exports = router;
