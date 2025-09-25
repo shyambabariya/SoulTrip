c5952715d72164cd432e2661a15b2e93e903b86d
@MaD Max <shyambabariyaa@gmail.com>
2025-09-25 23:17:17 +0530

    review controller added

---
+++ controllers/review.js
@@ -0,0 +1,20 @@
+const Listing = require("../models/listing");
+const Review = require("../models/review");
+
+module.exports.createReview = async (req, res) => {
+  let listing = await Listing.findById(req.params.id);
+  let review = new Review(req.body.review);
+  (review.author = req.user._id), listing.reviews.push(review);
+  await review.save();
+  await listing.save();
+  req.flash("success", "New Review Added Successfully!");
+  res.redirect(`/listings/${listing._id}`);
+};
+
+module.exports.destroyReview = async (req, res) => {
+  let { id, reviewId } = req.params;
+  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  req.flash("success", "Review Deleted Successfully!");
+  res.redirect(`/listings/${id}`);
+};
