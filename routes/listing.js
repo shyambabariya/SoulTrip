bcdeb4e4591a43a7e9173ab6c7728de7e511ed38
@MaD Max <shyambabariyaa@gmail.com>
2025-09-26 22:57:43 +0530

    listing route changed

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
 const { validateListing, isLoggedIn, isOwner } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 //******************************************************************************************
 // Routes
@@ -14,7 +17,12 @@ const listingController = require("../controllers/listing.js");
 router
   .route("/")
   .get(wrapAsync(listingController.index))
-  .post(isLoggedIn,validateListing, wrapAsync(listingController.createListing));
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
 
 // Create Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
@@ -26,7 +34,12 @@ router.get("/new", isLoggedIn, listingController.renderNewForm);
 router
   .route("/:id")
   .get(wrapAsync(listingController.showListing))
-  .put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
   .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 // Update Route
