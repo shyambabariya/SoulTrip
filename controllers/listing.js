687cc46f6baac18e9adcad5138ab264b477c8058
@MaD Max <shyambabariyaa@gmail.com>
2025-09-27 19:58:17 +0530

    file url save to listing

--- controllers/listing.js
+++ controllers/listing.js
@@ -23,8 +23,11 @@ module.exports.createListing = async (req, res) => {
   //Above way is more longer so we use shorter way to do the same things
 
   // for it we use object's key:value pair
+  let url = req.file.path;
+  let filename = req.file.filename;
   let newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
+  newListing.image = {url,filename};
   await newListing.save();
   req.flash("success", "New Listing Added Successfully!");
   res.redirect("/listings");
