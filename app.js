f023bdd88c1ec3c82fc84e16ed31cc489bb90fdf
@MaD Max <shyambabariyaa@gmail.com>
2025-09-27 19:59:45 +0530

    env file access

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // Requring the things here
 // ******************************************************************************************
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config();
+}
+console.log(process.env);
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
